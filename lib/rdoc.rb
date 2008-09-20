d5379b2dbe0498c1f2253fdad8af05a66e23a119
Designing Patterns <technical.inquiries@designingpatterns.com>
2008-09-20 15:23:23 +0000

    Prepare the trunk for version 2.2.1.

--- lib/rdoc.rb
+++ lib/rdoc.rb
@@ -372,7 +372,7 @@ module RDoc
   ##
   # RDoc version you are using
 
-  VERSION = "2.2.0"
+  VERSION = "2.2.1"
 
   ##
   # Name of the dotfile that contains the description of files to be processed
