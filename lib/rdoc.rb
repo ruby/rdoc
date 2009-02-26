bd49a905830751d894b2479f98330e632cb3c0c6
Eric Hodel <drbrain@segment7.net>
2009-02-26 21:26:59 +0000

    Bump version

--- lib/rdoc.rb
+++ lib/rdoc.rb
@@ -382,7 +382,7 @@ module RDoc
   ##
   # RDoc version you are using
 
-  VERSION = "2.4.0"
+  VERSION = "2.4.1"
 
   ##
   # Name of the dotfile that contains the description of files to be processed
