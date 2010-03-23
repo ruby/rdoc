64cbda6c7167350264a6ade91359f6e539f73275
Eric Hodel <drbrain@segment7.net>
2010-03-23 23:33:00 +0000

    Fix tests

--- test/test_rdoc_options.rb
+++ test/test_rdoc_options.rb
@@ -8,23 +8,32 @@ class TestRDocOptions < MiniTest::Unit::TestCase
     @options = RDoc::Options.new
   end
 
-  def test_parse_bogus
-    out, err = capture_io do
-      assert_raises SystemExit do
-        @options.parse %w[--bogus]
-      end
-    end
-
-    assert_match %r%^Usage: %, err
-    assert_match %r%^invalid option: --bogus%, err
-  end
-
   def test_parse_ignore_invalid
     out, err = capture_io do
       @options.parse %w[--ignore-invalid --bogus]
     end
 
     refute_match %r%^Usage: %, err
+    assert_match %r%^invalid option: --bogus%, err
+  end
+
+  def test_parse_ignore_invalid_default
+    out, err = capture_io do
+      @options.parse %w[--bogus]
+    end
+
+    refute_match %r%^Usage: %, err
+    assert_match %r%^invalid option: --bogus%, err
+  end
+
+  def test_parse_ignore_invalid_no
+    out, err = capture_io do
+      assert_raises SystemExit do
+        @options.parse %w[--no-ignore-invalid --bogus]
+      end
+    end
+
+    assert_match %r%^Usage: %, err
     assert_match %r%^invalid option: --bogus%, err
   end
 
