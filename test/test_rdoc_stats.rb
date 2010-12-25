a436badbcb6f90ae5c1b2d244a858aa82b13b608
Eric Hodel <drbrain@segment7.net>
2010-12-24 22:39:32 -0800

    Add test for pull request 1

---
+++ test/test_rdoc_stats.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'minitest/autorun'
+require 'rdoc/stats'
+require 'rdoc/code_objects'
+require 'rdoc/markup'
+require 'rdoc/parser'
+
+class TestRDocStats < MiniTest::Unit::TestCase
+
+  def setup
+    RDoc::TopLevel.reset
+
+    @s = RDoc::Stats.new 0
+  end
+
+  def test_report_constant_alias
+    tl = RDoc::TopLevel.new 'fake.rb'
+    mod = tl.add_module RDoc::NormalModule, 'M'
+
+    c = tl.add_class RDoc::NormalClass, 'C'
+    mod.add_constant c
+
+    ca = RDoc::Constant.new 'CA', nil, nil
+    ca.is_alias_for = c
+
+    tl.add_constant ca
+
+    RDoc::TopLevel.complete :public
+
+    report = @s.report
+
+    flunk
+  end
+
+end
+
