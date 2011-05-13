a7780af0216f4e7a94d5f7d4bee64752e9cf137d
phiggins <pete@peterhiggins.org>
2011-05-12 23:58:06 -0700

    Add failing test case for Rdoc::Parser::C.
    This was causing rdoc to miss documentation on some parts of ruby
    implemented in C, for example zlib.c and win32ole.c.

--- test/test_rdoc_parser_c.rb
+++ test/test_rdoc_parser_c.rb
@@ -929,6 +929,37 @@ Init_IO(void) {
     assert read_method.singleton
   end
 
+  def test_define_method_with_prototype
+    content = <<-EOF
+static VALUE rb_io_s_read(int, VALUE*, VALUE);
+
+/*Method Comment! */
+static VALUE
+rb_io_s_read(argc, argv, io)
+    int argc;
+    VALUE *argv;
+    VALUE io;
+{
+}
+
+void
+Init_IO(void) {
+    /*
+     * a comment for class Foo on rb_define_class
+     */
+    VALUE rb_cIO = rb_define_class("IO", rb_cObject);
+    rb_define_singleton_method(rb_cIO, "read", rb_io_s_read, -1);
+}
+    EOF
+
+    klass = util_get_class content, 'rb_cIO'
+    read_method = klass.method_list.first
+    assert_equal "read", read_method.name
+    assert_equal "Method Comment!   ", read_method.comment
+    assert_equal "rb_io_s_read", read_method.c_function
+    assert read_method.singleton
+  end
+
   def test_define_method_private
     content = <<-EOF
 /*Method Comment! */
