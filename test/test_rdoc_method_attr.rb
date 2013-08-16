b2efb6c4e404e11da5d8510696ea7dd51c3c6d95
Eric Hodel <drbrain@segment7.net>
2013-08-16 14:13:53 -0700

    Test MethodAttr#is_alias_for
    This is the basis for testing storing method aliases in RDoc::Store

--- test/test_rdoc_method_attr.rb
+++ test/test_rdoc_method_attr.rb
@@ -120,6 +120,10 @@ class TestRDocMethodAttr < XrefTestCase
     assert_equal 'C1::m', @c1__m.full_name
   end
 
+  def test_is_alias_for
+    assert_equal @c2_b, @c2_a.is_alias_for
+  end
+
   def test_output_name
     assert_equal '#m',  @c1_m.output_name(@c1)
     assert_equal '::m', @c1__m.output_name(@c1)
