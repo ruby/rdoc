d3f27c78ef925ce69df9154273c3c3a7304b206d
Kouhei Sutou <kou@clear-code.com>
2013-09-26 23:03:19 +0900

    i18n: Extract MessageExtractor to another file

--- lib/rdoc/generator/pot.rb
+++ lib/rdoc/generator/pot.rb
@@ -39,60 +39,7 @@ class RDoc::Generator::POT
     extractor.extract
   end
 
-  class MessageExtractor
-    def initialize(store)
-      @store = store
-      @po = PO.new
-    end
-
-    def extract
-      @store.all_classes_and_modules.each do |klass|
-        extract_from_klass(klass)
-      end
-      @po
-    end
-
-    private
-    def extract_from_klass(klass)
-      extract_text(klass.comment_location, klass.full_name)
-
-      klass.each_section do |section, constants, attributes|
-        extract_text(section.title ,"#{klass.full_name}: section title")
-        section.comments.each do |comment|
-          extract_text(comment, "#{klass.full_name}: #{section.title}")
-        end
-      end
-
-      klass.each_constant do |constant|
-        extract_text(constant.comment, constant.full_name)
-      end
-
-      klass.each_attribute do |attribute|
-        extract_text(attribute.comment, attribute.full_name)
-      end
-
-      klass.each_method do |method|
-        extract_text(method.comment, method.full_name)
-      end
-    end
-
-    def extract_text(text, comment, location=nil)
-      return if text.nil?
-
-      options = {
-        :extracted_comment => comment,
-        :references => [location].compact,
-      }
-      i18n_text = RDoc::I18n::Text.new(text)
-      i18n_text.extract_messages do |part|
-        @po.add(entry(part[:paragraph], options))
-      end
-    end
-
-    def entry(msgid, options)
-      POEntry.new(msgid, options)
-    end
-  end
+  autoload :MessageExtractor, 'rdoc/generator/pot/message_extractor'
 
   class PO
     def initialize
