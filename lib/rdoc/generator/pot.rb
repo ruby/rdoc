efa76e51a6f0f1d67cd2153f7b005593e6f4cbd7
Kouhei Sutou <kou@clear-code.com>
2013-09-26 23:06:44 +0900

    i18n: Extract PO to another file

--- lib/rdoc/generator/pot.rb
+++ lib/rdoc/generator/pot.rb
@@ -40,74 +40,7 @@ class RDoc::Generator::POT
   end
 
   autoload :MessageExtractor, 'rdoc/generator/pot/message_extractor'
-
-  class PO
-    def initialize
-      @entries = {}
-      add_header
-    end
-
-    def add(entry)
-      existing_entry = @entries[entry.msgid]
-      if existing_entry
-        entry = existing_entry.merge(entry)
-      end
-      @entries[entry.msgid] = entry
-    end
-
-    def to_s
-      po = ''
-      sort_entries.each do |entry|
-        po << "\n" unless po.empty?
-        po << entry.to_s
-      end
-      po
-    end
-
-    private
-    def add_header
-      add(header_entry)
-    end
-
-    def header_entry
-      comment = <<-COMMENT
-SOME DESCRIPTIVE TITLE.
-Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
-This file is distributed under the same license as the PACKAGE package.
-FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
-
-      COMMENT
-
-      content = <<-CONTENT
-Project-Id-Version: PACKAGE VERSEION
-Report-Msgid-Bugs-To: 
-PO-Revision-Date: YEAR-MO_DA HO:MI+ZONE
-Last-Translator: FULL NAME <EMAIL@ADDRESS>
-Language-Team: LANGUAGE <LL@li.org>
-Language: 
-MIME-Version: 1.0
-Content-Type: text/plain; charset=CHARSET
-Content-Transfer-Encoding: 8bit
-Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;
-      CONTENT
-
-      POEntry.new('',
-                  :msgstr => content,
-                  :translator_comment => comment,
-                  :flags => ['fuzzy'])
-    end
-
-    def sort_entries
-      headers, messages = @entries.values.partition do |entry|
-        entry.msgid.empty?
-      end
-      # TODO: sort by location
-      sorted_messages = messages.sort_by do |entry|
-        entry.msgid
-      end
-      headers + sorted_messages
-    end
-  end
+  autoload :PO,               'rdoc/generator/pot/po'
 
   class POEntry
     attr_reader :msgid
