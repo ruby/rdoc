f161ae60207b30c7f31f39c85606a86dd8426528
Zachary Scott <e@zzak.io>
2014-11-21 20:55:15 -0800

    Flatten class /cc https://github.com/rdoc/rdoc/pull/332#discussion_r20754697

--- lib/rdoc/ri/task.rb
+++ lib/rdoc/ri/task.rb
@@ -6,23 +6,21 @@ end unless defined?(RDoc)
 
 require 'rdoc/task'
 
-module RDoc::RI
-  class Task < RDoc::Task
-    def clobber_task_description
-      "Remove RDoc RI data files"
-    end
+class RDoc::RI::Task < RDoc::Task
+  def clobber_task_description
+    "Remove RDoc RI data files"
+  end
 
-    def defaults
-      super
-      @rdoc_dir = '.rdoc'
-    end
+  def defaults
+    super
+    @rdoc_dir = '.rdoc'
+  end
 
-    def rdoc_task_description
-      'Build RDoc RI data files'
-    end
+  def rdoc_task_description
+    'Build RDoc RI data files'
+  end
 
-    def rerdoc_task_description
-      'Rebuild RDoc RI data files'
-    end
+  def rerdoc_task_description
+    'Rebuild RDoc RI data files'
   end
 end
