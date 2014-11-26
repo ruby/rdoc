55fbd8947faf33b41289976b9296e6279f83eaa1
Eric Hodel <drbrain@segment7.net>
2014-11-26 11:29:03 -0800

    Add missing RDoc::SingleClass#aref_prefix
    This was missing and not implemented which caused errors for users that
    documented methods via the singleton class.
    
    Fixes #311

--- test/test_rdoc_single_class.rb
+++ test/test_rdoc_single_class.rb
@@ -2,6 +2,12 @@ require File.expand_path '../xref_test_case', __FILE__
 
 class TestRDocSingleClass < XrefTestCase
 
+  def test_aref_prefix
+    c = RDoc::SingleClass.new 'C'
+
+    assert_equal 'sclass', c.aref_prefix
+  end
+
   def test_definition
     c = RDoc::SingleClass.new 'C'
 
