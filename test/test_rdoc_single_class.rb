44ab9014097cc38cad0840f7ff0088bb4200f5fb
Eric Hodel <drbrain@segment7.net>
2014-11-26 11:30:23 -0800

    Refactor common setup in TestRDocSingleClass

--- test/test_rdoc_single_class.rb
+++ test/test_rdoc_single_class.rb
@@ -2,16 +2,18 @@ require File.expand_path '../xref_test_case', __FILE__
 
 class TestRDocSingleClass < XrefTestCase
 
-  def test_aref_prefix
-    c = RDoc::SingleClass.new 'C'
+  def setup
+    super
 
-    assert_equal 'sclass', c.aref_prefix
+    @c = RDoc::SingleClass.new 'C'
+  end
+
+  def test_aref_prefix
+    assert_equal 'sclass', @c.aref_prefix
   end
 
   def test_definition
-    c = RDoc::SingleClass.new 'C'
-
-    assert_equal 'class << C', c.definition
+    assert_equal 'class << C', @c.definition
   end
 
 end
