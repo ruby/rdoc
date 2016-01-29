3e51d4ac945f2b9145dced56464d03c95c1eaa0b
windwiny <windwiny.ubt@gmail.com>
2016-01-29 23:32:14 +0800

    Apply those patch to ~> 3.12 23c276 5cfb39
    RubyInstaller use rdoc(~>3.12) generate document.

--- lib/rdoc/context.rb
+++ lib/rdoc/context.rb
@@ -144,6 +144,7 @@ class RDoc::Context < RDoc::CodeObject
   # Contexts are sorted by full_name
 
   def <=>(other)
+    return nil unless RDoc::CodeObject === other
     full_name <=> other.full_name
   end
 
