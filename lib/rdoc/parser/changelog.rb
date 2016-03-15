869202ffecd6c4d033124a486703ee494c1dd169
Yusuke Endoh <mame@ruby-lang.org>
2016-03-15 22:04:01 +0900

    Cache the result of Time.parse
    Time.parse is painfully slow.

--- lib/rdoc/parser/changelog.rb
+++ lib/rdoc/parser/changelog.rb
@@ -101,9 +101,11 @@ class RDoc::Parser::ChangeLog < RDoc::Parser
   # Groups +entries+ by date.
 
   def group_entries entries
+    @time_cache ||= {}
     entries.group_by do |title, _|
       begin
-        Time.parse(title).strftime '%Y-%m-%d'
+        time = @time_cache[title]
+        (time || Time.parse(title)).strftime '%Y-%m-%d'
       rescue NoMethodError, ArgumentError
         time, = title.split '  ', 2
         Time.parse(time).strftime '%Y-%m-%d'
@@ -127,6 +129,7 @@ class RDoc::Parser::ChangeLog < RDoc::Parser
   #        'README.EXT.ja:  ditto']]
 
   def parse_entries
+    @time_cache ||= {}
     entries = []
     entry_name = nil
     entry_body = []
@@ -142,6 +145,7 @@ class RDoc::Parser::ChangeLog < RDoc::Parser
 
         begin
           time = Time.parse entry_name
+          @time_cache[entry_name] = time
           # HACK Ruby 1.8 does not raise ArgumentError for Time.parse "Other"
           entry_name = nil unless entry_name =~ /#{time.year}/
         rescue NoMethodError
@@ -184,6 +188,7 @@ class RDoc::Parser::ChangeLog < RDoc::Parser
   # Converts the ChangeLog into an RDoc::Markup::Document
 
   def scan
+    @time_cache = {}
     entries = parse_entries
     grouped_entries = group_entries entries
 
