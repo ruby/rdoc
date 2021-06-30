ad17c3a9867b76596ef2f9af02568488c181535a
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2021-07-01 00:18:15 +0200

    Relative loading for easier development
    This patch makes sure we only load relative code. Hence when coding or
    testing rdoc, we'll be sure to always be using the correct code.
    
    Discussion started at https://github.com/ruby/rdoc/pull/817.
    
    Signed-off-by: Ulysse Buonomo <buonomo.ulysse@gmail.com>

--- test/rdoc/support/test_case.rb
+++ test/rdoc/support/test_case.rb
@@ -26,7 +26,7 @@ require 'tempfile'
 require 'tmpdir'
 require 'stringio'
 
-require 'rdoc'
+require_relative '../../../lib/rdoc'
 
 ##
 # RDoc::TestCase is an abstract TestCase to provide common setup and teardown
--- test/rdoc/test_rdoc_include.rb
+++ test/rdoc/test_rdoc_include.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require File.expand_path '../xref_test_case', __FILE__
+require_relative 'xref_test_case'
 
 class TestRDocInclude < XrefTestCase
 
@@ -106,4 +106,3 @@ class TestRDocInclude < XrefTestCase
   end
 
 end
-
