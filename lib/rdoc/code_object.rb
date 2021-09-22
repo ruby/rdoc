79629dcc5dcafc165f46d419ea5ace9addb5b668
Mike Dalessio <mike.dalessio@gmail.com>
2021-09-22 17:49:43 -0400

    Embed mixed-in methods and constants with `--embed-mixins`
    When `--embed-mixins` option is set:
    
    - methods from an `extend`ed module are documented as singleton methods
    - attrs from an `extend`ed module are documented as class attributes
    - methods from an `include`ed module are documented as instance methods
    - attrs from an `include`ed module are documented as instance attributes
    - constants from an `include`ed module are documented
    
    Sections are created when needed, and Darkfish's template annotates
    each of these mixed-in CodeObjects. We also respect the mixin methods'
    visibility.
    
    This feature is inspired by Yard's option of the same name.

--- lib/rdoc/code_object.rb
+++ lib/rdoc/code_object.rb
@@ -97,6 +97,11 @@ class RDoc::CodeObject
   attr_accessor :viewer
 
   ##
+  # When mixed-in to a class, this points to the Context in which it was originally defined.
+
+  attr_accessor :mixin_from
+
+  ##
   # Creates a new CodeObject that will document itself and its children
 
   def initialize
