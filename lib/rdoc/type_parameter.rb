4bf209ee8556f9729a477e49a1ad23da5726e37f
Sushanth Sathesh Rao <57192414+raosush@users.noreply.github.com>
2022-09-11 21:51:15 +0530

    Marshal: Load, dump for TypeParameter

--- lib/rdoc/type_parameter.rb
+++ lib/rdoc/type_parameter.rb
@@ -2,11 +2,30 @@ module RDoc
   class TypeParameter < CodeObject
     attr_reader :name, :variance, :unchecked, :upper_bound
 
+    MARSHAL_VERSION = 3 # :nodoc:
+
     def initialize(name, variance, unchecked = false, upper_bound = nil)
       @name = name
       @variance = variance
       @unchecked = unchecked
       @upper_bound = upper_bound
+    end
+
+    def marshal_load(array)
+      @name = array[1]
+      @variance = array[2]
+      @unchecked = array[3]
+      @upper_bound = array[4]
+    end
+
+    def marshal_dump
+      [
+        MARSHAL_VERSION,
+        @name,
+        @variance,
+        @unchecked,
+        @upper_bound
+      ]
     end
 
     def ==(other)
