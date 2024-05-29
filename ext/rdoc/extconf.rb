e2df5cd5720e87fd5a2cc74900011d0ce2ad6513
Stan Lo <stan.lo@shopify.com>
2024-05-29 19:01:41 +0100

    Allow installing RDoc from a git source
    Co-authored-by: Vinicius Stock <vinistock@users.noreply.github.com>

---
+++ ext/rdoc/extconf.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Generate all parse files using racc and kpeg. This is not necessary for regular gem installation, but it is when
+# installing RDoc from the git source. Without this, the generated parse files would not exist and RDoc would not work
+system("rake generate") if Dir.exist?(File.join("..", "..", ".git"))
+
+# RDoc doesn't actually have a native extension, but a Makefile needs to exist in order to successfully install the gem
+require "mkmf"
+create_makefile("rdoc/rdoc")
