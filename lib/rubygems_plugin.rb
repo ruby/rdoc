42b86d1251f20a56f9540dbece06c1d1b711a802
masaru.terada <masaru.terada@enigmo.co.jp>
2024-11-14 09:14:22 +0900

    add comment for rubygems_plugin.rb

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# If this file is exist, RDoc generates and removes documents by rubygems plugins.
+#
+# In follwing cases,
+# RubyGems directly exectute RDoc::RubygemsHook.generation_hook and RDoc::RubygemsHook#remove to generate and remove documents.
+#
+# - RDoc is used as a default gem.
+# - RDoc is a old version that doesn't have rubygems_plugin.rb.
+
 require_relative 'rdoc/rubygems_hook'
 
 # To install dependency libraries of RDoc, you need to run bundle install.
