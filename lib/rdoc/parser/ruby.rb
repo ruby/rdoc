b17e94846c87cb9d688cb4e484d0d6c3f4018b21
tompng <tomoyapenguin@gmail.com>
2025-01-31 22:27:26 +0900

    Escape directive-like document content

--- lib/rdoc/parser/ruby.rb
+++ lib/rdoc/parser/ruby.rb
@@ -120,7 +120,7 @@ require_relative 'ripper_state_lex'
 #   # :singleton-method: some_method!
 #
 # You can define arguments for metaprogrammed methods via either the
-# :call-seq:, :arg: or :args: directives.
+# \:call-seq:, :arg: or :args: directives.
 #
 # Additionally you can mark a method as an attribute by
 # using :attr:, :attr_reader:, :attr_writer: or :attr_accessor:.  Just like
