2e8fea617214de13babb6a1d5031b92d89f727af
Stan Lo <stan001212@gmail.com>
2025-03-23 10:48:36 +0000

    Improve extconf

--- ext/rdoc/extconf.rb
+++ ext/rdoc/extconf.rb
@@ -2,7 +2,11 @@
 
 # Generate all parse files using racc and kpeg. This is not necessary for regular gem installation, but it is when
 # installing RDoc from the git source. Without this, the generated parse files would not exist and RDoc would not work
-system("rake generate") if File.exist?("../../.git")
+Dir.chdir(File.expand_path("../..", __dir__)) do
+  if !File.exist?("lib/rdoc/markdown.rb") && Dir.exist?(".git")
+    system("rake generate", exception: true)
+  end
+end
 
 # RDoc doesn't actually have a native extension, but a Makefile needs to exist in order to successfully install the gem
 require "mkmf"
