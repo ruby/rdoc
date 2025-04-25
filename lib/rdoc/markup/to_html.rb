11ebd4e8c4ef5ccab8e43e134528e4d7b80a0830
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-04-25 21:01:48 +0900

    Tidy up comment indentation

--- lib/rdoc/markup/to_html.rb
+++ lib/rdoc/markup/to_html.rb
@@ -98,7 +98,7 @@ class RDoc::Markup::ToHtml < RDoc::Markup::Formatter
 
       gen_url CGI.escapeHTML(url), CGI.escapeHTML(text)
     when /^rdoc-image:/
-     # Split the string after "rdoc-image:" into url and alt.
+      # Split the string after "rdoc-image:" into url and alt.
       #   "path/to/image.jpg:alt text" => ["path/to/image.jpg", "alt text"]
       #   "http://example.com/path/to/image.jpg:alt text" => ["http://example.com/path/to/image.jpg", "alt text"]
       url, alt = $'.split(/:(?!\/)/, 2)
