ruby/rdoc

commit 894b2f1cd147a760449a52b621af7bf206452127

Image alt tag header formatting (#1320)
This allows us to parse the alt text from Markdown images, formatting a
header text correctly (using the alt text).

Without this PR, if a Markdown file has an image instead of a title,
that image will be used in the table of contents, which is unreadable
and undesirable.

Used in <https://github.com/rack/rack/pull/2313>.

---------

Co-authored-by: Stan Lo <stan001212@gmail.com>

lib/rdoc/markup/to_html.rb
@@ -98,7 +98,12 @@ class RDoc::Markup::ToHtml < RDoc::Markup::Formatter
 
       gen_url CGI.escapeHTML(url), CGI.escapeHTML(text)
     when /^rdoc-image:/
-      %[<img src=\"#{CGI.escapeHTML($')}\">]
+      url, alt = $'.split(":", 2) # Split the string after "rdoc-image:" into url and alt
+      if alt && !alt.empty?
+        %[<img src="#{CGI.escapeHTML(url)}" alt="#{CGI.escapeHTML(alt)}">]
+      else
+        %[<img src="#{CGI.escapeHTML(url)}">]
+      end
     when /\Ardoc-[a-z]+:/
       CGI.escapeHTML($')
     end

lib/rdoc/markdown.kpeg
@@ -992,9 +992,14 @@ Strike = &{ strike? }
          "~~"
          { strike a.join }
 
-# TODO alt text support
-Image = "!" ( ExplicitLink | ReferenceLink ):a
-        { "rdoc-image:#{a[/\[(.*)\]/, 1]}" }
+Image = "!" ExplicitLink:a
+        {
+          # Extract alt text and URL
+          alt_text = a[/\{(.*?)\}/, 1] || ""
+          url = a[/\[(.*?)\]/, 1] || ""
+
+          "rdoc-image:#{url}:#{alt_text}"
+        }
 
 Link =  ExplicitLink | ReferenceLink | AutoLink
 

lib/rdoc/markup/heading.rb
@@ -66,7 +66,13 @@ RDoc::Markup::Heading =
   # element.
 
   def plain_html
-    self.class.to_html.to_html(text.dup)
+    text = self.text.dup
+
+    if matched = text.match(/rdoc-image:[^:]+:(.*)/)
+      text = matched[1]
+    end
+
+    self.class.to_html.to_html(text)
   end
 
   def pretty_print q # :nodoc:

test/rdoc/test_rdoc_markdown.rb
@@ -511,7 +511,7 @@ heading
   def test_parse_image
     doc = parse "image ![alt text](path/to/image.jpg)"
 
-    expected = doc(para("image rdoc-image:path/to/image.jpg"))
+    expected = doc(para("image rdoc-image:path/to/image.jpg:alt text"))
 
     assert_equal expected, doc
   end
@@ -523,7 +523,7 @@ heading
 
     expected =
       doc(
-        para('{rdoc-image:path/to/image.jpg}[http://example.com]'))
+        para('{rdoc-image:path/to/image.jpg:alt text}[http://example.com]'))
 
     assert_equal expected, doc
   end

test/rdoc/test_rdoc_markup_heading.rb
@@ -26,4 +26,9 @@ class TestRDocMarkupHeading < RDoc::TestCase
     assert_equal 'Hello <strong>Friend</strong>!', @h.plain_html
   end
 
+  def test_plain_html_using_image_alt_as_text
+    h = RDoc::Markup::Heading.new 1, 'rdoc-image:foo.png:Hello World'
+
+    assert_equal 'Hello World', h.plain_html
+  end
 end

commit 5a98946b2bd3d5f7df8d33c654205a7301db25bd

Fix image tag with external source
When `rdoc-image` contains an external source, such as `https://example.com/path/to/image.svg`,
rendered `<img>` tag is broken. This fixes the bug.

For example, I found the problem on the IRB website, where a Markdown image is present as a source.

Rendered HTML on the website <https://ruby.github.io/irb/>:

```html
<a href="https://badge.fury.io/rb/irb"><img src="https" alt="//badge.fury.io/rb/irb.svg:Gem Version"></a>
```

Markdown source: <https://github.com/ruby/irb/blob/6349b03f64f173a9d31bc9042430e1134f3e1689/doc/Index.md?plain=1#L3>

```markdown
[![Gem Version](https://badge.fury.io/rb/irb.svg)](https://badge.fury.io/rb/irb)
```

Probably, this bug was introduced in the commit 894b2f1cd147a760449a52b621af7bf206452127 (v6.13.0).

lib/rdoc/markup/to_html.rb
@@ -98,7 +98,11 @@ class RDoc::Markup::ToHtml < RDoc::Markup::Formatter
 
       gen_url CGI.escapeHTML(url), CGI.escapeHTML(text)
     when /^rdoc-image:/
-      url, alt = $'.split(":", 2) # Split the string after "rdoc-image:" into url and alt
+      # Split the string after "rdoc-image:" into url and alt
+      url_alt = $'
+      split_pos = url_alt.index(%r{:[^/]})
+      url = split_pos ? url_alt[0, split_pos] : url_alt
+      alt = split_pos ? url_alt[split_pos + 1, url_alt.size] : nil
       if alt && !alt.empty?
         %[<img src="#{CGI.escapeHTML(url)}" alt="#{CGI.escapeHTML(alt)}">]
       else

test/rdoc/test_rdoc_markup_to_html.rb
@@ -749,6 +749,36 @@ EXPECTED
     assert_not_include result, "<script>"
   end
 
+  def test_convert_TIDYLINK_image_with_alt
+    result =
+      @to.convert '{rdoc-image:path/to/image.jpg:alt text}[http://example.com]'
+
+    expected =
+      "\n<p><a href=\"http://example.com\"><img src=\"path/to/image.jpg\" alt=\"alt text\"></a></p>\n"
+
+    assert_equal expected, result
+  end
+
+  def test_convert_TIDYLINK_image_external
+    result =
+      @to.convert '{rdoc-image:http://example.com/path/to/image.jpg}[http://example.com]'
+
+    expected =
+      "\n<p><a href=\"http://example.com\"><img src=\"http://example.com/path/to/image.jpg\"></a></p>\n"
+
+    assert_equal expected, result
+  end
+
+  def test_convert_TIDYLINK_image_external_with_alt
+    result =
+      @to.convert '{rdoc-image:http://example.com/path/to/image.jpg:alt text}[http://example.com]'
+
+    expected =
+      "\n<p><a href=\"http://example.com\"><img src=\"http://example.com/path/to/image.jpg\" alt=\"alt text\"></a></p>\n"
+
+    assert_equal expected, result
+  end
+
   def test_convert_TIDYLINK_rdoc_label
     result = @to.convert '{foo}[rdoc-label:foottext-1]'
 