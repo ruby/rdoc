61c1e8ac6d864be23e20da1aa877887e9c77b672
Vinicius Stock <vinistock@users.noreply.github.com>
2025-12-24 11:33:44 -0300

    Standardize table and heading markup elements (#1389)
    I noticed that Markup elements are currently not standardized in any way
    - even though the code does make some assumptions.
    
    For example, there are visitors that depend on all elements implementing
    `accept`, but no parent class or interface that will help guarantee
    every element implements it. Some elements are implemented as structs,
    while other are classes. Some have parent classes for shared
    functionality and APIs and others don't.
    
    I propose that we standardize the markup elements a bit more, so that we
    can have a common interface for them. It will make it easier for the
    RDoc to Markdown translator if we can enforce that every markup element
    has to be able to print itself in Markdown - and I believe the code will
    be easier to understand too.
    
    This PR creates a base `Markup::Element` class and I standardized
    `Heading` and `Table` as an example. I'm also finding it challenging to
    understand the logic with no clue about what types are and what is
    expected from each method, so I also propose we annotate some of these
    as documentation - even if we're not adopting type checking.
    
    If folks agree, I will continue to standardize more markup elements. I'm
    trying to document what each element represents along the way.

--- lib/rdoc/markup/heading.rb
+++ lib/rdoc/markup/heading.rb
@@ -1,84 +1,101 @@
 # frozen_string_literal: true
-##
-# A heading with a level (1-6) and text
 
-RDoc::Markup::Heading =
-  Struct.new :level, :text do
+module RDoc
+  class Markup
+    # A heading with a level (1-6) and text
+    #
+    #  RDoc syntax:
+    #   = Heading 1
+    #   == Heading 2
+    #   === Heading 3
+    #
+    #  Markdown syntax:
+    #   # Heading 1
+    #   ## Heading 2
+    #   ### Heading 3
+    class Heading < Element
+      #: String
+      attr_reader :text
 
-  @to_html = nil
-  @to_label = nil
+      #: Integer
+      attr_accessor :level
 
-  ##
-  # A singleton RDoc::Markup::ToLabel formatter for headings.
+      # A singleton RDoc::Markup::ToLabel formatter for headings.
+      #: () -> RDoc::Markup::ToLabel
+      def self.to_label
+        @to_label ||= Markup::ToLabel.new
+      end
 
-  def self.to_label
-    @to_label ||= RDoc::Markup::ToLabel.new
-  end
+      # A singleton plain HTML formatter for headings. Used for creating labels for the Table of Contents
+      #: () -> RDoc::Markup::ToHtml
+      def self.to_html
+        @to_html ||= begin
+          markup = Markup.new
+          markup.add_regexp_handling CrossReference::CROSSREF_REGEXP, :CROSSREF
 
-  ##
-  # A singleton plain HTML formatter for headings.  Used for creating labels
-  # for the Table of Contents
+          to_html = Markup::ToHtml.new nil
 
-  def self.to_html
-    return @to_html if @to_html
+          def to_html.handle_regexp_CROSSREF(target)
+            target.text.sub(/^\\/, '')
+          end
 
-    markup = RDoc::Markup.new
-    markup.add_regexp_handling RDoc::CrossReference::CROSSREF_REGEXP, :CROSSREF
+          to_html
+        end
+      end
 
-    @to_html = RDoc::Markup::ToHtml.new nil
+      #: (Integer, String) -> void
+      def initialize(level, text)
+        super()
 
-    def @to_html.handle_regexp_CROSSREF(target)
-      target.text.sub(/^\\/, '')
+        @level = level
+        @text = text
+      end
+
+      #: (Object) -> bool
+      def ==(other)
+        other.is_a?(Heading) && other.level == @level && other.text == @text
+      end
+
+      # @override
+      #: (untyped) -> void
+      def accept(visitor)
+        visitor.accept_heading(self)
+      end
+
+      # An HTML-safe anchor reference for this header.
+      #: () -> String
+      def aref
+        "label-#{self.class.to_label.convert text.dup}"
+      end
+
+      # Creates a fully-qualified label which will include the label from +context+. This helps keep ids unique in HTML.
+      #: (RDoc::Context?) -> String
+      def label(context = nil)
+        label = +""
+        label << "#{context.aref}-" if context&.respond_to?(:aref)
+        label << aref
+        label
+      end
+
+      # HTML markup of the text of this label without the surrounding header element.
+      #: () -> String
+      def plain_html
+        no_image_text = text
+
+        if matched = no_image_text.match(/rdoc-image:[^:]+:(.*)/)
+          no_image_text = matched[1]
+        end
+
+        self.class.to_html.to_html(no_image_text)
+      end
+
+      # @override
+      #: (PP) -> void
+      def pretty_print(q)
+        q.group 2, "[head: #{level} ", ']' do
+          q.pp text
+        end
+      end
     end
-
-    @to_html
   end
-
-  ##
-  # Calls #accept_heading on +visitor+
-
-  def accept(visitor)
-    visitor.accept_heading self
-  end
-
-  ##
-  # An HTML-safe anchor reference for this header.
-
-  def aref
-    "label-#{self.class.to_label.convert text.dup}"
-  end
-
-  ##
-  # Creates a fully-qualified label which will include the label from
-  # +context+.  This helps keep ids unique in HTML.
-
-  def label(context = nil)
-    label = aref
-
-    label = [context.aref, label].compact.join '-' if
-      context and context.respond_to? :aref
-
-    label
-  end
-
-  ##
-  # HTML markup of the text of this label without the surrounding header
-  # element.
-
-  def plain_html
-    text = self.text.dup
-
-    if matched = text.match(/rdoc-image:[^:]+:(.*)/)
-      text = matched[1]
-    end
-
-    self.class.to_html.to_html(text)
-  end
-
-  def pretty_print(q) # :nodoc:
-    q.group 2, "[head: #{level} ", ']' do
-      q.pp text
-    end
-  end
-
 end
