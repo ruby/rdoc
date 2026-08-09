c162cac17e7668dfa931a0d4c4f46c89c5103541
st0012 <stan001212@gmail.com>
2026-08-09 11:54:46 +0100

    Standardize List and ListItem markup
    Co-authored-by: Vinicius Stock <vinicius.stock@shopify.com>

--- lib/rdoc/markup/list.rb
+++ lib/rdoc/markup/list.rb
@@ -1,101 +1,93 @@
 # frozen_string_literal: true
-##
-# A List is a homogeneous set of ListItems.
-#
-# The supported list types include:
-#
-# :BULLET::
-#   An unordered list
-# :LABEL::
-#   An unordered definition list, but using an alternate RDoc::Markup syntax
-# :LALPHA::
-#   An ordered list using increasing lowercase English letters
-# :NOTE::
-#   An unordered definition list
-# :NUMBER::
-#   An ordered list using increasing Arabic numerals
-# :UALPHA::
-#   An ordered list using increasing uppercase English letters
-#
-# Definition lists behave like HTML definition lists.  Each list item can
-# describe multiple terms.  See RDoc::Markup::ListItem for how labels and
-# definition are stored as list items.
 
-class RDoc::Markup::List < RDoc::Markup::Element
+module RDoc
+  class Markup
+    # A List is a homogeneous set of ListItems.
+    #
+    # The supported list types include:
+    #
+    # :BULLET::
+    #   An unordered list
+    # :LABEL::
+    #   An unordered definition list, but using an alternate RDoc::Markup syntax
+    # :LALPHA::
+    #   An ordered list using increasing lowercase English letters
+    # :NOTE::
+    #   An unordered definition list
+    # :NUMBER::
+    #   An ordered list using increasing Arabic numerals
+    # :UALPHA::
+    #   An ordered list using increasing uppercase English letters
+    #
+    # Definition lists behave like HTML definition lists. Each list item can
+    # describe multiple terms. See RDoc::Markup::ListItem for how labels and
+    # definitions are stored as list items.
+    class List < Element
+      # The list's type
+      #: Symbol?
+      attr_accessor :type
 
-  ##
-  # The list's type
+      # Items in the list
+      #: Array[ListItem]
+      attr_reader :items
 
-  attr_accessor :type
+      # Creates a new list of +type+ with +items+. Valid list types are:
+      # +:BULLET+, +:LABEL+, +:LALPHA+, +:NOTE+, +:NUMBER+, +:UALPHA+
+      #: (Symbol?, *ListItem) -> void
+      def initialize(type = nil, *items)
+        @type = type
+        @items = items
+      end
 
-  ##
-  # Items in the list
+      # Appends +item+ to the list
+      #: (ListItem) -> void
+      def <<(item)
+        @items << item
+      end
 
-  attr_reader :items
+      #: (top) -> bool
+      def ==(other) # :nodoc:
+        self.class == other.class &&
+          @type == other.type &&
+          @items == other.items
+      end
 
-  ##
-  # Creates a new list of +type+ with +items+.  Valid list types are:
-  # +:BULLET+, +:LABEL+, +:LALPHA+, +:NOTE+, +:NUMBER+, +:UALPHA+
+      # Runs this list and all its #items through +visitor+
+      # @override
+      #: (untyped) -> void
+      def accept(visitor)
+        visitor.accept_list_start(self)
+        @items.each { |item| item.accept(visitor) }
+        visitor.accept_list_end(self)
+      end
 
-  def initialize(type = nil, *items)
-    @type = type
-    @items = []
-    @items.concat items
-  end
+      # Is the list empty?
+      #: () -> bool
+      def empty?
+        @items.empty?
+      end
 
-  ##
-  # Appends +item+ to the list
+      # Returns the last item in the list
+      #: () -> ListItem?
+      def last
+        @items.last
+      end
 
-  def <<(item)
-    @items << item
-  end
+      # @override
+      #: (PP) -> void
+      def pretty_print(q) # :nodoc:
+        q.group(2, "[list: #{@type} ", ']') do
+          q.seplist(@items) do |item|
+            q.pp(item)
+          end
+        end
+      end
 
-  def ==(other) # :nodoc:
-    self.class == other.class and
-      @type == other.type and
-      @items == other.items
-  end
-
-  ##
-  # Runs this list and all its #items through +visitor+
-
-  def accept(visitor)
-    visitor.accept_list_start self
-
-    @items.each do |item|
-      item.accept visitor
-    end
-
-    visitor.accept_list_end self
-  end
-
-  ##
-  # Is the list empty?
-
-  def empty?
-    @items.empty?
-  end
-
-  ##
-  # Returns the last item in the list
-
-  def last
-    @items.last
-  end
-
-  def pretty_print(q) # :nodoc:
-    q.group 2, "[list: #{@type} ", ']' do
-      q.seplist @items do |item|
-        q.pp item
+      # Appends +items+ to the list
+      #: (*ListItem) -> void
+      def push(*items)
+        @items.concat(items)
       end
     end
   end
-
-  ##
-  # Appends +items+ to the list
-
-  def push(*items)
-    @items.concat items
-  end
-
 end
