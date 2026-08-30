c72644446cce8b1c95859d4fa57ab0a310edc1f0
nick evans <nick@rubinick.dev>
2026-08-30 14:32:16 -0400

    Don't create meta attributes with no name
    This updates inferred attribute name parsing for the `:attr:`,
    `:attr_reader:`, `:attr_writer:`, and `:attr_accessor:` directives, to
    avoid creating unnamed attributes from unparsable arguments.
    
    Previously, an unnamed (nil) attribute would be created.
    Now, any non-string/non-symbol arguments are simply ignored.

--- lib/rdoc/parser/ruby.rb
+++ lib/rdoc/parser/ruby.rb
@@ -412,7 +412,7 @@ module RDoc
           case directive
           when 'attr', 'attr_reader', 'attr_writer', 'attr_accessor'
             attributes = [param] if param
-            attributes ||= call_node_name_arguments(node) if is_call_node
+            attributes ||= call_node_name_arguments(node).compact if is_call_node
             rw = directive == 'attr_writer' ? 'W' : directive == 'attr_accessor' ? 'RW' : 'R'
           when 'method'
             method_name = param if param
