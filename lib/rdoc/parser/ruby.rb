ab0170d75e9ea500932514ae3271d3c2e954c0de
nicholas a. evans <nick@rubinick.dev>
2026-09-11 08:38:04 -0400

    Allow less strict attribute argument parsing (#1793)
    This updates argument parsing for the `attr`, `attr_reader`,
    `attr_writer`, and `attr_accessor` methods, so they behave more like
    rdoc 7.2's parser.
    
    * Previously, the prism parser's
    `_visit_call_attr_reader_writer_accessor` only recognized attributes
    when _all_ arguments are symbols.
    * Now, all symbol or string arguments are used and all others are
    ignored.
    
    This also updates inferred attribute name parsing for the `:attr:`,
    `:attr_reader:`, `:attr_writer:`, and `:attr_accessor:` directives, to
    avoid creating unnamed attributes from unparsable arguments.
    
    * Previously, non-string/non-symbol arguments would create unnamed
    (`nil`) attributes.
    * Now, any non-string/non-symbol arguments are ignored.
    
    As an example, the rdoc for `Net::IMAP::Config` intentionally took
    advantage of the looser parsing done by rdoc 7.2. That class redefines
    `attr_reader`, `attr_writer` and `attr_accessor` to add keyword
    arguments for type validation/coercion and defaults:
    
    ```ruby
          # Seconds to wait until a connection is opened.
          #
          # Applied separately for establishing TCP connection and starting a TLS
          # connection.
          #
          # If the IMAP object cannot open a connection within this time,
          # it raises a Net::OpenTimeout exception.
          #
          # See Net::IMAP.new and Net::IMAP#starttls.
          #
          # The default value is +30+ seconds.
          attr_accessor :open_timeout, type: Integer, default: 30
    ```
    
    rdoc 7.2 simply ignored the unknown keyword args, and parses this no
    differently from `attr_accessor :open_timeout.`
    
    Fixes #1790.

--- lib/rdoc/parser/ruby.rb
+++ lib/rdoc/parser/ruby.rb
@@ -412,7 +412,7 @@ module RDoc
           case directive
           when 'attr', 'attr_reader', 'attr_writer', 'attr_accessor'
             attributes = [param] if param
-            attributes ||= call_node_name_arguments(node) if is_call_node
+            attributes ||= call_node_name_arguments(node).compact if is_call_node
             rw = directive == 'attr_writer' ? 'W' : directive == 'attr_accessor' ? 'RW' : 'R'
           when 'method'
             method_name = param if param
@@ -1215,6 +1215,11 @@ module RDoc
           names.all? ? names : nil
         end
 
+        def call_node_name_arguments(call_node)
+          names = @scanner.call_node_name_arguments(call_node).compact
+          names unless names.empty?
+        end
+
         def symbol_arguments(call_node)
           arguments_node = call_node.arguments
           return unless arguments_node && arguments_node.arguments.all? { |arg| arg.is_a?(Prism::SymbolNode)}
@@ -1329,8 +1334,8 @@ module RDoc
 
         def _visit_call_attr_reader_writer_accessor(call_node, rw)
           return if @scanner.in_proc_block
-          names = symbol_arguments(call_node)
-          @scanner.add_attributes(names.map(&:to_s), rw, call_node.location.start_line) if names
+          names = call_node_name_arguments(call_node)
+          @scanner.add_attributes(names, rw, call_node.location.start_line) if names
         end
 
         class MethodSignatureVisitor < Prism::Visitor # :nodoc:
